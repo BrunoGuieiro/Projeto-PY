8e56ace2960f8e00f39a9d968c66c00adfbac39b
RogerERSouza <rogerrocha.rocha2017@gmail.com>
2026-03-27 11:42:11 -0300

    Update cadastrar.py
    Cadastro, registro, mudança de nome de classe

--- Backend/cadastrar.py
+++ Backend/cadastrar.py
@@ -1,32 +1,67 @@
-class Produto:
+class Peca:
     def __init__(self, id, nome, custo, preco, estoque):
         self.id = id
         self.nome = nome
         self.custo = custo
         self.preco = preco
         self.estoque = estoque
-        self.data = data
 
-produtos = []
+pecas = []
 
-def cadastrar_produto():
-    
+def cadastrar_peca():
     nome = input("Nome: ")
-    custo = input("Custo: ")
-    preco = input("Preco: ")
-    estoque = input("Estoque: ")
-    novo_produto = Produto(
-        id = len(produtos) + 1,
-        nome = nome,
-        custo = custo,
-        preco = preco,
-        estoque = estoque,
+    custo = float(input("Custo: "))
+    preco = float(input("Preco: "))
+    estoque = int(input("Estoque: "))
+    nova_peca = Peca(
+        id=len(pecas) + 1,
+        nome=nome,
+        custo=custo,
+        preco=preco,
+        estoque=estoque
     )
-    produtos.append(novo_produto)
+    pecas.append(nova_peca)
 
+def registrar_producao():
+    for p in pecas:
+        print(p.id, p.nome, p.custo, p.preco, p.estoque)
+    id_digitado = int(input("Id da peça: "))
+    peca_encontrada = False
+    for p in pecas:
+        if p.id == id_digitado:
+            valor_aumentar = int(input("Insira quanto quer aumentar no estoque: "))
+            p.estoque += valor_aumentar
+            peca_encontrada = True
+            break
+    if not peca_encontrada:
+        print("Peça não encontrada!")
 
+def vender_peca():
+    for p in pecas:
+        print(p.id, p.nome, p.custo, p.preco, p.estoque)
+    id_digitado = int(input("Id da peça: "))
+    peca_encontrada = False
+    for p in pecas:
+        if p.id == id_digitado:
+            valor_venda = int(input("Insira a quantidade que deseja vender: "))
+            if valor_venda > p.estoque:
+                print("Estoque insuficiente!")
+            else:
+                p.estoque -= valor_venda
+            peca_encontrada = True
+            break
+    if not peca_encontrada:
+        print("Peça não encontrada!")
 
-cadastrar_produto()
-
-for p in produtos:
+# Teste
+cadastrar_peca()
+for p in pecas:
     print(p.id, p.nome, p.custo, p.preco, p.estoque)
+
+registrar_producao()
+for p in pecas:
+    print(p.id, p.nome, p.estoque)
+
+vender_peca()
+for p in pecas:
+    print(p.id, p.nome, p.estoque)
